a418488f4021047d949d3b05fa9c52c6800507e0
Eric Flores <flores.eric94@gmail.com>
2023-02-01 10:11:03 -0800

    Added script
    -object script to move the platform

--- Zumbie Rush/Assets/Scripts/Object.cs
+++ Zumbie Rush/Assets/Scripts/Object.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Object : MonoBehaviour
+
+    
 {
+    [SerializeField] float objSpeed = 1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,6 @@ public class Object : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.right * (objSpeed * Time.deltaTime));
     }
 }
