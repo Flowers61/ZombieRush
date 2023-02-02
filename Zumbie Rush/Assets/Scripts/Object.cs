6209ef745fec92498ff84a6ec67475b1bf9437b1
Eric Flores <flores.eric94@gmail.com>
2023-02-02 09:21:27 -0800

    object script
    added continuous platform loop

--- Zumbie Rush/Assets/Scripts/Object.cs
+++ Zumbie Rush/Assets/Scripts/Object.cs
@@ -7,6 +7,7 @@ public class Object : MonoBehaviour
     
 {
     [SerializeField] float objSpeed = 1;
+    private float resetPos = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,5 +19,11 @@ public class Object : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector3.right * (objSpeed * Time.deltaTime));
+
+        if(transform.position.x >= resetPos)
+        {
+            Vector3 newPos = new Vector3(-105, transform.position.y, transform.position.z);
+            transform.position = newPos;
+        }
     }
 }
